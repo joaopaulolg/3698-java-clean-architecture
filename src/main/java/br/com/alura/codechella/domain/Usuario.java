ea2f691729cfa387e56784dd7064abfdcf6d74b3
João Paulo L. Gonçalves <joaopaulolg@gmail.com>
2024-09-22 22:28:24 -0300

    Aula 1
    Aula 1

---
+++ src/main/java/br/com/alura/codechella/domain/Usuario.java
@@ -0,0 +1,57 @@
+package br.com.alura.codechella.domain;
+
+import java.time.LocalDate;
+
+public class Usuario {
+
+    private Long id;
+
+    private String cpf;
+
+    private String nome;
+
+    private LocalDate nascimento;
+
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
